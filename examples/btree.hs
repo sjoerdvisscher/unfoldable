0a69eefd62cbca244c46d541c81a7d29a1782707
Sjoerd Visscher <sjoerd@w3future.com>
2012-04-23 01:03:13 +0200

    Split the examples in 2 files.

---
+++ examples/btree.hs
@@ -0,0 +1,23 @@
+import Control.Applicative
+import Data.Unfoldable
+
+import Data.Maybe
+import System.Random
+
+
+data TB a = LB a | BB (TB (a, a)) deriving Show
+
+instance Unfoldable TB where
+  unfold fa = choose
+    [ LB <$> fa
+    , BB <$> unfold ((,) <$> fa <*> fa)
+    ]
+
+btree8 :: TB Int
+btree8 = fromJust $ fromList [0..7]
+
+btreeShapes :: [TB ()]
+btreeShapes = take 5 unfold_
+
+randomBTree :: IO (TB Bool)
+randomBTree = getStdRandom randomDefault
--- examples/tree.hs
+++ examples/tree.hs
@@ -22,21 +22,3 @@ treeShapes = take 10 unfoldBF_
 
 randomTree :: IO (Tree Bool)
 randomTree = getStdRandom randomDefault
-
-
-data TB a = LB a | BB (TB (a, a)) deriving Show
-
-instance Unfoldable TB where
-  unfold fa = choose
-    [ LB <$> fa
-    , BB <$> unfold ((,) <$> fa <*> fa)
-    ]
-
-btree8 :: TB Int
-btree8 = fromJust $ fromList [0..7]
-
-btreeShapes :: [TB ()]
-btreeShapes = take 5 unfold_
-
-randomBTree :: IO (TB Bool)
-randomBTree = getStdRandom randomDefault
