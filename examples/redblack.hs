b9e880a9c9ac59f358123e85a7eee89905539d9c
Sjoerd Visscher <sjoerd@w3future.com>
2012-05-21 17:59:11 +0200

    Added an example how to use the NumConst newtype.

--- examples/redblack.hs
+++ examples/redblack.hs
@@ -13,6 +13,7 @@ import Data.Unfolder
 
 import Data.Maybe
 import System.Random
+import Data.List (intercalate)
 
 -- red-black tree implementation adapted from https://gist.github.com/2660297
 data Nat = Zero | Succ Nat deriving (Eq, Ord, Show)
@@ -64,11 +65,43 @@ instance Unfoldable (Node Black n) => Unfoldable (Node Black (Succ n)) where
 instance Unfoldable (Node Black n) => Unfoldable (Node Red n) where
   unfold fa = choose [ R <$> unfold fa <*> fa <*> unfold fa ]
 
-rbtree8 :: RedBlackTree Int
-rbtree8 = fromJust $ fromList [0..7]
+rbtree :: Int -> RedBlackTree Int
+rbtree l = fromJust $ fromList [0..l]
 
-rbtreeShapes :: [RedBlackTree ()]
-rbtreeShapes = take 20 unfold_
+rbtreeShapes :: Int -> [RedBlackTree ()]
+rbtreeShapes d = limitDepth d unfold_
 
 randomRBTree :: IO (RedBlackTree Bool)
 randomRBTree = getStdRandom randomDefault
+
+newtype Formula = Formula [Integer]
+instance Show Formula where
+  show (Formula xs) 
+    | all (== 0) xs = "0"
+    | otherwise = intercalate " + " $ map showOne $ filter ((/=0) . fst) $ zip xs [0..]
+        where
+          showOne (x, 0) = show x
+          showOne (1, 1) = "x"
+          showOne (x, 1) = show x ++ "x"
+          showOne (1, n) = "x^" ++ show n
+          showOne (x, n) = show x ++ "x^" ++ show n
+
+add, mul :: [Integer] -> [Integer] -> [Integer]
+add xs [] = xs
+add [] ys = ys
+add (x:xs) (y:ys) = (x + y) : add xs ys
+mul _ [] = []
+mul [] _ = []
+mul (x:xs) (y:ys) = (x * y) : add (map (x*) ys) (add (map (y*) xs) (0 : mul xs ys))
+
+instance Num Formula where
+  fromInteger x = Formula [x]
+  Formula xs + Formula ys = Formula (add xs ys)
+  Formula xs * Formula ys = Formula (mul xs ys)
+
+x :: Formula
+x = Formula [0, 1]
+
+-- See http://oeis.org/A001137
+rbFormula :: Int -> NumConst Formula (RedBlackTree a)
+rbFormula d = ala (limitDepth d) unfold (NumConst x)
